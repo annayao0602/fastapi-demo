d476ecadf09906550fc2ced39780b6da0255057d
AnnaYao <annayao@Annas-MacBook-Air.local>
2024-11-19 17:06:53 -0500

    changes made

--- app/main.py
+++ app/main.py
@@ -5,8 +5,25 @@ from typing import Optional
 from pydantic import BaseModel
 import json
 import os
+import mysql.connector
+from mysql.connector import Error
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "zzz2bx"
+
+db= mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 
 @app.get("/")  # zone apex
 def zone_apex():
@@ -24,6 +41,30 @@ def multiply(c: int, d: int):
 def square(e: int):
     return{"square": e * e} 
 
-@app.get("/data")
-def return():
-    return("returning some data")
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+@app.get('/songs')
+def get_songs():
+    query = "SELECT * FROM songs ORDER BY id;"  # Query to fetch all songs
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]  # Extract column names
+        results = cur.fetchall()  # Fetch all rows
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(headers, result)))  # Map columns to values
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
